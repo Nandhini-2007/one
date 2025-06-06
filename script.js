d7b11d6552688dbe88287c65020f5cc68edd6b9c
Nandhini-2007 <rnandhini157@gmail.com>
2025-06-06 15:01:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function sub(a, b){
+    return a - b;
+}
+console.log(sub(5, 10));
+console.log(sub(3, 7));
+
+const choices = ['rock','paper','scissor']
+let computer = choices[Math.floor(Math.random() * 3)]
+console.log(computer);
